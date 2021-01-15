f4a77af2b66457cbd06933f1d8952b87c9913e5d
bernardo <bernardo.fichera@gmail.com>
2021-01-15 10:10:27 +0100

    Work in progress.

--- src/kernel_lib/kernels/Rbf.hpp
+++ src/kernel_lib/kernels/Rbf.hpp
@@ -39,17 +39,57 @@ namespace kernel_lib {
                 return *this;
             }
 
-            void logKernel(Eigen::MatrixXd& k, const Eigen::MatrixXd& x, const Eigen::MatrixXd& y) const
+            // void logKernel(Eigen::MatrixXd& k, const Eigen::MatrixXd& x, const Eigen::MatrixXd& y) const
+            // {
+            //     if (_type & CovarianceType::SPHERICAL) {
+            //         spherical(k, x, y);
+            //     }
+            //     else if (_type & CovarianceType::DIAGONAL) {
+            //         diagonal(k, x, y);
+            //     }
+            //     else if (_type & CovarianceType::FULL) {
+            //         full(k, x, y);
+            //     }
+            // }
+
+            std::unique_ptr<Eigen::MatrixXd> logKernel(const Eigen::MatrixXd& x, const Eigen::MatrixXd& y) const
             {
+                REQUIRED_DIMENSION(x.cols() == y.cols(), "Y must have the same dimension of X")
+
+                Eigen::MatrixXd k(x.rows(), y.rows());
+
                 if (_type & CovarianceType::SPHERICAL) {
-                    spherical(k, x, y);
+                    REQUIRED_DIMENSION(_sigma.rows() == 1, "Sigma requires dimension 1")
+
+                    double sig = -0.5 / std::pow(_sigma(0, 0), 2);
+
+#pragma omp parallel for collapse(2)
+                    for (size_t j = 0; j < y.rows(); j++)
+                        for (size_t i = 0; i < x.rows(); i++)
+                            k(i, j) = (x.row(i) - y.row(j)).squaredNorm() * sig;
                 }
                 else if (_type & CovarianceType::DIAGONAL) {
-                    diagonal(k, x, y);
+                    REQUIRED_DIMENSION(_sigma.rows() == x.cols(), "Sigma requires dimension equal to the number of features")
+
+                    Eigen::Array<double, 1, Eigen::Dynamic> sig = -0.5 * _sigma.transpose().array().pow(2).inverse();
+
+#pragma omp parallel for collapse(2)
+                    for (size_t j = 0; j < y.rows(); j++)
+                        for (size_t i = 0; i < x.rows(); i++)
+                            k(i, j) = exp(((x.row(i) - y.row(j)).array().square() * sig).sum());
                 }
                 else if (_type & CovarianceType::FULL) {
-                    full(k, x, y);
+                    REQUIRED_DIMENSION(_sigma.rows() == std::pow(x.cols(), 2), "Sigma requires dimension equal to squared number of features")
+
+                    Eigen::LLT<Eigen::MatrixXd, Eigen::Upper> U = tools::upperCholesky(_sigma.reshaped(x.cols(), x.cols()));
+
+#pragma omp parallel for collapse(2)
+                    for (size_t j = 0; j < y.rows(); j++)
+                        for (size_t i = 0; i < x.rows(); i++)
+                            k(i, j) = std::exp(U.solve((x.row(i) - y.row(j)).transpose()).squaredNorm() * -0.5);
                 }
+
+                return std::make_unique<Eigen::MatrixXd>(k);
             }
 
         protected:
@@ -61,20 +101,21 @@ namespace kernel_lib {
 
             Eigen::MatrixXd kernel(const Eigen::MatrixXd& x, const Eigen::MatrixXd& y) const override
             {
-                REQUIRED_DIMENSION(x.cols() == y.cols(), "Y must have the same dimension of X")
+                // REQUIRED_DIMENSION(x.cols() == y.cols(), "Y must have the same dimension of X")
 
-                Eigen::MatrixXd k(x.rows(), y.rows());
+                // Eigen::MatrixXd k(x.rows(), y.rows());
 
                 double sf2 = std::exp(2 * std::log(AbstractKernel<Params>::_sigma_f)), sn2 = std::exp(2 * std::log(AbstractKernel<Params>::_sigma_n));
 
-                logKernel(k, x, y);
+                std::unique_ptr<Eigen::MatrixXd> k = logKernel(x, y);
 
-                k = sf2 * k.array().exp();
+                // *k = k->array().exp();
+                // k->array().exp(); // *k = sf2 *
 
-                if (&x == &y)
-                    k.diagonal().array() += sn2 + 1e-8;
+                // if (&x == &y)
+                //     k->diagonal().array() += sn2 + 1e-8;
 
-                return k;
+                return k->array().exp();
             }
 
             Eigen::MatrixXd gradient(const Eigen::MatrixXd& x, const Eigen::MatrixXd& y) const override
