b8d3e8a1b973382de632a661e4f1deb23f425d99
bernardo <bernardo.fichera@gmail.com>
2021-05-08 11:30:36 +0200

    Removed sequence of benchmarks (multi-threading not working).

--- src/benchmarks/bench_rbf.cpp
+++ src/benchmarks/bench_rbf.cpp
@@ -20,35 +20,16 @@ struct Params {
 
 int main(int argc, char const* argv[])
 {
-    std::chrono::seconds wait(2);
-
-    // using Kernel_t = kernels::Rbf<Params>;
     using Kernel_t = kernels::SquaredExp<Params>;
     Kernel_t k;
 
-    size_t dim = 2, num_samples = 10000;
+    size_t dim = 2, num_samples = 20000;
     Eigen::MatrixXd X = Eigen::MatrixXd::Random(num_samples, dim), Y = Eigen::MatrixXd::Random(num_samples, dim);
 
     std::cout << "BENCHMARK: Kernel evaluation" << std::endl;
     {
         utils_cpp::Timer timer;
         k(X, X);
-    }
-
-    std::this_thread::sleep_for(wait);
-
-    std::cout << "BENCHMARK: Kernel gradient" << std::endl;
-    {
-        utils_cpp::Timer timer;
-        k.gradient(X, X);
-    }
-
-    std::this_thread::sleep_for(wait);
-
-    std::cout << "BENCHMARK: Kernel params gradient" << std::endl;
-    {
-        utils_cpp::Timer timer;
-        k.gradientParams(X, X);
     }
 
     return 0;
