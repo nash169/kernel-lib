a5031243c017f23ec21d98739c50e356af7305b8
bernardo <bernardo.fichera@gmail.com>
2021-07-12 13:40:12 +0200

    Improved general structure leveraging on Eigen::Ref. To do: template specialization for multiEval method.

--- src/kernel_lib/utils/Expansion.hpp
+++ src/kernel_lib/utils/Expansion.hpp
@@ -1,11 +1,21 @@
 #ifndef KERNELLIB_EXPANSION_UTILS_HPP
 #define KERNELLIB_EXPANSION_UTILS_HPP
 
+#include "type_name_rt.hpp"
+
 namespace kernel_lib {
     namespace defaults {
         struct expansion {
         };
     } // namespace defaults
+
+    namespace kernels {
+        template <typename Params>
+        class SquaredExp;
+
+        template <typename Params, typename Function>
+        class RiemannSqExp;
+    } // namespace kernels
 
     namespace utils {
         template <typename Params, typename Kernel>
@@ -13,36 +23,72 @@ namespace kernel_lib {
         public:
             Expansion() : _kernel() {}
 
-            double operator()(const Eigen::VectorXd& x)
+            template <int Size>
+            EIGEN_ALWAYS_INLINE double operator()(const Eigen::Matrix<double, Size, 1>& x) const
             {
                 double r = 0;
 
+                // Can parallelize this?
+                // https://pages.tacc.utexas.edu/~eijkhout/pcse/html/omp-data.html
                 for (size_t i = 0; i < _reference.rows(); i++)
-                    r += _weights(i) * _kernel(_reference(i), x);
+                    r += _params(i) * _kernel.template kernelImpl<Size>(_reference.row(i), x);
 
                 return r;
             }
 
-            Eigen::VectorXd multiEval(const Eigen::MatrixXd& x)
+            template <int Size>
+            Eigen::VectorXd multiEval(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x) const
             {
                 Eigen::VectorXd r(x.rows());
 
+#pragma omp parallel for
                 for (size_t i = 0; i < r.rows(); i++)
-                    r(i) = (*this)(x.row(i));
+                    r(i) = this->operator()<Size>(x.row(i));
 
                 return r;
             }
 
-            Expansion& setReference(const Eigen::MatrixXd& reference)
+            template <int Size>
+            Eigen::VectorXd multiEval2(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x) const
+            {
+                return _kernel.gram(_reference, x).transpose() * _params;
+            }
+
+            // Check https://stackoverflow.com/questions/47035541/specialize-only-a-part-of-one-method-of-a-template-class
+            //       https://stackoverflow.com/questions/12877420/specialization-of-template-class-method
+            Eigen::VectorXd temp(const Eigen::MatrixXd& x) const
+            {
+                if constexpr (std::is_same_v<Kernel, kernels::SquaredExp<Params>>) {
+                    std::cout << "Hello" << std::endl;
+                }
+
+                // Check https://github.com/willwray/type_name
+                std::cout << type_name_str<Kernel>() << std::endl;
+
+                Eigen::VectorXd r(x.rows());
+                return r;
+            }
+
+            const Eigen::MatrixXd& reference() const
+            {
+                return _reference;
+            }
+
+            const Eigen::VectorXd& params() const
+            {
+                return _params;
+            }
+
+            virtual Expansion& setReference(const Eigen::MatrixXd& reference)
             {
                 _reference = reference;
 
                 return *this;
             }
 
-            Expansion& setWeights(const Eigen::VectorXd& weights)
+            Expansion& setParams(const Eigen::VectorXd& params)
             {
-                _weights = weights;
+                _params = params;
 
                 return *this;
             }
@@ -53,11 +99,23 @@ namespace kernel_lib {
             }
 
         protected:
+            /* Kernel */
             Kernel _kernel;
 
-            Eigen::VectorXd _weights;
+            /* Parameters */
+            Eigen::VectorXd _params;
+
+            /* Referece points */
             Eigen::MatrixXd _reference;
         };
+
+        // template <>
+        // Eigen::VectorXd Expansion<float, kernels::SquaredExp<float>>::temp(const Eigen::MatrixXd& x) const
+        // {
+        //     Eigen::VectorXd r(x.rows());
+
+        //     return r;
+        // }
     } // namespace utils
 } // namespace kernel_lib
 
