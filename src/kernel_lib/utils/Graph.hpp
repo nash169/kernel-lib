826b5f75af7bda3098827e6498155015b3eb7441
Bernardo <bernardo.fichera@gmail.com>
2021-07-20 15:28:04 +0200

    Improved structure and added weighted version of k-nearest and epsilon neighborhoods algorithms.

--- src/kernel_lib/utils/Graph.hpp
+++ src/kernel_lib/utils/Graph.hpp
@@ -12,12 +12,8 @@ namespace kernel_lib {
         public:
             Graph() {}
 
-            const Eigen::SparseMatrix<int, Eigen::RowMajor>& graph() const
-            {
-                return _G;
-            }
-
-            Graph& epsNeighborhoods(const Eigen::MatrixXd& x, const double& eps = 1)
+            // Epsilon neighborhoods
+            Eigen::SparseMatrix<int, Eigen::RowMajor> epsNeighborhoods(const Eigen::MatrixXd& x, const double& eps = 1)
             {
                 // Eigen triplet
                 std::vector<Eigen::Triplet<int>> tripletList;
@@ -28,29 +24,67 @@ namespace kernel_lib {
                             tripletList.push_back(Eigen::Triplet<int>(i, j, 1));
                     }
 
-                _G = Eigen::SparseMatrix<int, Eigen::RowMajor>(x.rows(), x.rows());
-                _G.setFromTriplets(tripletList.begin(), tripletList.end());
-
-                return *this;
+                return graph(tripletList, x.rows());
             }
 
-            Graph& kNearest(const Eigen::MatrixXd& x, const size_t& k = 1)
+            // Weighted Epsilon neighborhoods
+            template <typename Kernel>
+            Eigen::SparseMatrix<double, Eigen::RowMajor> epsNeighborhoodsWeighted(const Eigen::MatrixXd& x, const Kernel& weight, const double& eps = 1)
+            {
+                // Eigen triplet
+                std::vector<Eigen::Triplet<double>> tripletList;
+
+                for (size_t i = 0; i < x.rows(); i++)
+                    for (size_t j = 0; j < x.rows(); j++) {
+                        if ((x.row(i) - x.row(j)).squaredNorm() <= eps)
+                            tripletList.push_back(Eigen::Triplet<double>(i, j, weight(x.row(i), x.row(j))));
+                    }
+
+                return graph(tripletList, x.rows());
+            }
+
+            // K-Nearest
+            Eigen::SparseMatrix<int, Eigen::RowMajor> kNearest(const Eigen::MatrixXd& x, const size_t& k = 1, const size_t& exclude = 0)
             {
                 // Eigen triplet
                 std::vector<Eigen::Triplet<int>> tripletList;
 
                 for (size_t i = 0; i < x.rows(); i++) {
                     std::vector<size_t> idx = sort(x.rowwise() - x.row(i));
-                    for (size_t j = 0; j < k; j++)
+                    for (size_t j = 0 + exclude; j < k; j++)
                         tripletList.push_back(Eigen::Triplet<int>(i, idx[j], 1));
                 }
 
-                _G = Eigen::SparseMatrix<int, Eigen::RowMajor>(x.rows(), x.rows());
-                _G.setFromTriplets(tripletList.begin(), tripletList.end());
-
-                return *this;
+                return graph(tripletList, x.rows());
             }
 
+            // Weighted K-Nearest
+            template <typename Kernel>
+            Eigen::SparseMatrix<double, Eigen::RowMajor> kNearestWeighted(const Eigen::MatrixXd& x, const Kernel& weight, const size_t& k = 1, const size_t& exclude = 0)
+            {
+                // Eigen triplet
+                std::vector<Eigen::Triplet<double>> tripletList;
+
+                for (size_t i = 0; i < x.rows(); i++) {
+                    std::vector<size_t> idx = sort(x.rowwise() - x.row(i));
+                    for (size_t j = 0 + exclude; j < k; j++)
+                        tripletList.push_back(Eigen::Triplet<double>(i, idx[j], weight(x.row(i), x.row(idx[j]))));
+                }
+
+                return graph(tripletList, x.rows());
+            }
+
+            // Create graph from triplet
+            template <typename T>
+            Eigen::SparseMatrix<T, Eigen::RowMajor> graph(const std::vector<Eigen::Triplet<T>>& tripletList, const size_t& size)
+            {
+                Eigen::SparseMatrix<T, Eigen::RowMajor> G = Eigen::SparseMatrix<T, Eigen::RowMajor>(size, size);
+                G.setFromTriplets(tripletList.begin(), tripletList.end());
+
+                return G;
+            }
+
+        protected:
             std::vector<size_t> sort(const Eigen::MatrixXd& v)
             {
                 // initialize original index locations
@@ -61,9 +95,6 @@ namespace kernel_lib {
 
                 return idx;
             }
-
-        protected:
-            Eigen::SparseMatrix<int, Eigen::RowMajor> _G;
         };
 
     } // namespace utils
