7046557f72ec4c711ff6e19818552baa5b964027
bernardo <bernardo.fichera@gmail.com>
2022-04-22 19:16:46 +0200

    Various improvements.

--- src/kernel_lib/kernels/RiemannSqExp.hpp
+++ src/kernel_lib/kernels/RiemannSqExp.hpp
@@ -51,59 +51,30 @@ namespace kernel_lib {
             using KernelFunction = Function;
             RiemannSqExp() : _l(std::exp(Params::riemann_exp_sq::l())) {}
 
-            /* Overload kernel for handling single sample evaluation */
-            // template <int Size>
-            // EIGEN_ALWAYS_INLINE double kernel(const Eigen::Matrix<double, Size, 1>& x, const Eigen::Matrix<double, Size, 1>& y) const
-            // {
-            //     double r = 0, n = 0;
-
-            //     // this has to parallelized
-            //     for (auto& pair : _pairs) {
-            //         n += std::exp(-0.5 * std::pow(_l, 2));
-            //         r += std::exp(-0.5 * std::pow(_l, 2) * pair.first) * pair.second(x) * pair.second(y);
-            //     }
-
-            //     return r / n;
-            // }
-
             template <typename Derived>
             EIGEN_ALWAYS_INLINE double kernel(const Eigen::MatrixBase<Derived>& x, const Eigen::MatrixBase<Derived>& y) const
             {
-                double r = 0, n = 0;
+                double r = 0;
 
                 // this has to parallelized
-                for (auto& pair : _pairs) {
-                    n += std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-                    r += std::exp(-0.5 * std::pow(_l, 2) * pair.first) * pair.second.template operator() < (Derived::RowsAtCompileTime == 1) ? Derived::ColsAtCompileTime : Derived::RowsAtCompileTime > (x)*pair.second.template operator() < (Derived::RowsAtCompileTime == 1) ? Derived::ColsAtCompileTime
-                                                                                                                                                                                                                                                                                 : Derived::RowsAtCompileTime > (y);
-                }
+                for (size_t i = 0; i < _d.rows(); i++)
+                    r += _s(i)
+                                * _f[i].template operator()
+                            < (Derived::RowsAtCompileTime == 1)
+                        ? Derived::ColsAtCompileTime
+                        : Derived::RowsAtCompileTime > (x)*_f[i].template operator() < (Derived::RowsAtCompileTime == 1) ? Derived::ColsAtCompileTime
+                                                                                                                         : Derived::RowsAtCompileTime > (y);
 
-                return r / n;
+                return r;
             }
 
             template <int Size>
             EIGEN_DEVICE_FUNC Eigen::MatrixXd gram(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x, const Eigen::Matrix<double, Eigen::Dynamic, Size>& y) const
             {
-                size_t x_samples = x.rows(), y_samples = y.rows();
-                Eigen::MatrixXd k = Eigen::MatrixXd::Zero(x_samples, y_samples);
-                double n = 0;
+                Eigen::MatrixXd k = Eigen::MatrixXd::Zero(x.rows(), y.rows());
 
-                // #pragma omp parallel
-                for (auto& pair : _pairs) {
-                    n += std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-                    k += std::exp(-0.5 * std::pow(_l, 2) * pair.first) * pair.second.multiEval(x) * pair.second.multiEval(y).transpose();
-                }
-
-                //                 for (auto& pair : _pairs) {
-                //                     n += std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-                //                     Eigen::VectorXd fx = pair.second.multiEval(x), fy = pair.second.multiEval(y);
-                // #pragma omp parallel for collapse(2)
-                //                     for (size_t j = 0; j < y_samples; j++)
-                //                         for (size_t i = 0; i < x_samples; i++)
-                //                             k(i, j) += std::exp(-0.5 * std::pow(_l, 2) * pair.first) * fx(i) * fy(j);
-                //                 }
-
-                k *= _sf2 / n;
+                for (size_t i = 0; i < _d.rows(); i++)
+                    k += _s(i) * _f[i].multiEval(x) * _f[i].multiEval(y).transpose();
 
                 if (x.data() == y.data())
                     k.diagonal().array() += _sn2;
@@ -114,45 +85,24 @@ namespace kernel_lib {
             template <typename Derived>
             EIGEN_ALWAYS_INLINE auto gradient(const Eigen::MatrixBase<Derived>& x, const Eigen::MatrixBase<Derived>& y, const size_t& i = 1) const
             {
-                return Eigen::VectorXd::Zero(x.size());
+                Eigen::VectorXd g = Eigen::VectorXd::Zero(x.rows());
+
+                for (size_t i = 0; i < _d.rows(); i++)
+                    g += _s(i) * ((i) ? _f[i].template grad<SIZE>(x) * _f[i].template operator()<SIZE>(y) : _f[i].template grad<SIZE>(y) * _f[i].template operator()<SIZE>(x));
+
+                return g;
             }
 
             template <typename Derived>
             EIGEN_ALWAYS_INLINE double gradientParams(const Eigen::MatrixBase<Derived>& x, const Eigen::MatrixBase<Derived>& y, const size_t& i = 1) const
             {
-                double r = 0, dr = 0, dn = 0, n = 0, cn = 0, cr = 0;
+                double g = 0;
+                Eigen::VectorXd ds = spectralGrad();
 
-                for (auto& pair : _pairs) {
-                    // Repeated term
-                    cn = std::exp(-0.5 * std::pow(_l, 2) * pair.first);
+                for (size_t i = 0; i < _d.rows(); i++)
+                    g += ds(i) * _f[i].template operator()<SIZE>(x) * _f[i].template operator()<SIZE>(y);
 
-                    // Normalization term
-                    n += cn;
-
-                    // Normalization derivative
-                    dn += pair.first * cn;
-
-                    // Repeated term
-                    cr = cn * pair.second.template operator()
-                            < (Derived::RowsAtCompileTime == 1)
-                        ? Derived::ColsAtCompileTime
-                        : Derived::RowsAtCompileTime > (x)*pair.second.template operator() < (Derived::RowsAtCompileTime == 1) ? Derived::ColsAtCompileTime
-                                                                                                                               : Derived::RowsAtCompileTime > (y);
-                    // Kernel
-                    r += cr;
-
-                    // Kernel derivative
-                    dr += pair.first * cr;
-                }
-
-                return std::pow(_l, 2) / n * (dn * r / n - dr);
-
-                // for (auto& i : _f.eigenPair()) {
-                //     n += std::exp(-0.5 * std::pow(_l, 2));
-                //     r -= std::pow(_l, 2) * i.first * std::exp(-0.5 * std::pow(_l, 2) * i.first) * _f(x, i.first) * _f(y, i.first);
-                // }
-
-                // return r / n / _f.eigenPair().size();
+                return g;
             }
 
             template <int Size>
@@ -189,28 +139,23 @@ namespace kernel_lib {
                 return grad;
             }
 
-            // EigenFunction& eigenFunction()
-            // {
-            //     return _f;
-            // }
-
-            // template <typename... Args>
-            // void addEigenPair(const double& eigenvalue, const EigenFunction& eigenfunction, Args... args)
-            // {
-            //     _d.push_back(eigenvalue);
-            //     _f.push_back(eigenfunction);
-
-            //     if constexpr (sizeof...(args) > 0)
-            //         addEigenPair(args...);
-            // }
-
             template <typename... Args>
             void addPair(const double& value, const Function& function, Args... args)
             {
                 _pairs.insert(std::make_pair(value, function));
 
+                // Insert eigenvalue
+                _d.conservativeResize(_d.rows() + 1);
+                _d(_d.rows() - 1) = value;
+
+                // Insert eigenfunction
+                _f.push_back(function);
+
                 if constexpr (sizeof...(args) > 0)
                     addPair(args...);
+
+                // Update spectral density
+                spectral();
             }
 
         protected:
@@ -220,13 +165,30 @@ namespace kernel_lib {
             using AbstractKernel<Params, RiemannSqExp<Params, Function>>::_sf2;
             using AbstractKernel<Params, RiemannSqExp<Params, Function>>::_sn2;
 
-            // std::vector<double> _d;
-            // std::vector<EigenFunction> _f;
+            // Eigenvalues and Spectral density
+            Eigen::VectorXd _d, _s;
+
+            // Eigenfunctions
+            std::vector<Function> _f;
 
             // Eigen pairs (eigenvalues, eigenfunctions)
             // EigenFunction _f;
             // This map can become ordered
             std::unordered_map<double, Function> _pairs;
+
+            // Spectral density
+            virtual void spectral()
+            {
+                _s = (-0.5 * std::pow(_l, 2) * _d).array().exp();
+                _s *= _sf2 / _s.sum();
+            }
+
+            // Gradient spectral density
+            virtual Eigen::VectorXd spectralGrad() const
+            {
+                Eigen::VectorXd ds = _l * _s;
+                return _s.array() * (ds.sum() - ds.array());
+            }
 
             Eigen::VectorXd parameters() const override
             {
