3a15acaa1d924b7cecf9011dc51c6ac7913ac260
bernardo <bernardo.fichera@gmail.com>
2022-04-24 19:30:12 +0200

    Fixed and numerically checked Riemann gradient wrt input and params.

--- src/kernel_lib/kernels/RiemannSqExp.hpp
+++ src/kernel_lib/kernels/RiemannSqExp.hpp
@@ -25,10 +25,6 @@
 #ifndef KERNELLIB_KERNELS_RIEMANNSQEXP_HPP
 #define KERNELLIB_KERNELS_RIEMANNSQEXP_HPP
 
-#define VECTORIZED_EXPANSION
-
-#include <unordered_map>
-
 #include "kernel_lib/kernels/AbstractKernel.hpp"
 #include "kernel_lib/tools/macros.hpp"
 #include "kernel_lib/tools/math.hpp"
@@ -57,29 +53,10 @@ namespace kernel_lib {
                 double r = 0;
 
                 // this has to parallelized
-                for (size_t i = 0; i < _d.rows(); i++)
-                    r += _s(i)
-                                * _f[i].template operator()
-                            < (Derived::RowsAtCompileTime == 1)
-                        ? Derived::ColsAtCompileTime
-                        : Derived::RowsAtCompileTime > (x)*_f[i].template operator() < (Derived::RowsAtCompileTime == 1) ? Derived::ColsAtCompileTime
-                                                                                                                         : Derived::RowsAtCompileTime > (y);
+                for (size_t k = 0; k < _d.rows(); k++)
+                    r += _s(k) * _f[k].template operator()<SIZE>(x) * _f[k].template operator()<SIZE>(y);
 
                 return r;
-            }
-
-            template <int Size>
-            EIGEN_DEVICE_FUNC Eigen::MatrixXd gram(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x, const Eigen::Matrix<double, Eigen::Dynamic, Size>& y) const
-            {
-                Eigen::MatrixXd k = Eigen::MatrixXd::Zero(x.rows(), y.rows());
-
-                for (size_t i = 0; i < _d.rows(); i++)
-                    k += _s(i) * _f[i].multiEval(x) * _f[i].multiEval(y).transpose();
-
-                if (x.data() == y.data())
-                    k.diagonal().array() += _sn2;
-
-                return k;
             }
 
             template <typename Derived>
@@ -87,8 +64,8 @@ namespace kernel_lib {
             {
                 Eigen::VectorXd g = Eigen::VectorXd::Zero(x.rows());
 
-                for (size_t i = 0; i < _d.rows(); i++)
-                    g += _s(i) * ((i) ? _f[i].template grad<SIZE>(x) * _f[i].template operator()<SIZE>(y) : _f[i].template grad<SIZE>(y) * _f[i].template operator()<SIZE>(x));
+                for (size_t k = 0; k < _d.rows(); k++)
+                    g += _s(k) * ((i) ? _f[k].template grad<SIZE>(x) * _f[k].template operator()<SIZE>(y) : _f[k].template grad<SIZE>(y) * _f[k].template operator()<SIZE>(x));
 
                 return g;
             }
@@ -106,44 +83,47 @@ namespace kernel_lib {
             }
 
             template <int Size>
+            EIGEN_DEVICE_FUNC Eigen::MatrixXd gram(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x, const Eigen::Matrix<double, Eigen::Dynamic, Size>& y) const
+            {
+                Eigen::MatrixXd k = Eigen::MatrixXd::Zero(x.rows(), y.rows());
+
+                for (size_t i = 0; i < _d.rows(); i++)
+                    k += _sf2 * _s(i) * _f[i].multiEval(x) * _f[i].multiEval(y).transpose();
+
+                if (x.data() == y.data())
+                    k.diagonal().array() += _sn2;
+
+                return k;
+            }
+
+            template <int Size>
             EIGEN_DEVICE_FUNC Eigen::MatrixXd gramGradParams(const Eigen::Matrix<double, Eigen::Dynamic, Size>& x, const Eigen::Matrix<double, Eigen::Dynamic, Size>& y) const
             {
-                size_t x_samples = x.rows(), y_samples = y.rows();
-
-                Eigen::MatrixXd grad(x_samples * y_samples, 3), r(x_samples, y_samples);
-                double n = 0, dn = 0, c = 0;
-
-                for (auto& pair : _pairs) {
-                    n += std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-                    dn += pair.first * std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-                }
+                size_t x_num = x.rows(), y_num = y.rows();
+                Eigen::MatrixXd g = Eigen::MatrixXd::Zero(x.rows() * y.rows(), 3),
+                                k(x.rows(), y.rows());
+                Eigen::VectorXd ds = spectralGrad();
 
                 // #pragma omp parallel
-                for (auto& pair : _pairs) {
-                    c = std::exp(-0.5 * std::pow(_l, 2) * pair.first);
-
-                    r = c / n * pair.second.multiEval(x) * pair.second.multiEval(y).transpose();
-
-                    grad.col(0) += 2 * _sf2 * Eigen::Map<Eigen::VectorXd>(r.data(), x_samples * y_samples);
-
-                    grad.col(2) += _sf2 * std::pow(_l, 2) * (dn / n - pair.first) * Eigen::Map<Eigen::VectorXd>(r.data(), x_samples * y_samples);
+                for (size_t i = 0; i < _d.rows(); i++) {
+                    k = _sf2 * _f[i].multiEval(x) * _f[i].multiEval(y).transpose();
+                    g.col(0) += 2 * _s(i) * Eigen::Map<Eigen::VectorXd>(k.data(), x_num * y_num);
+                    g.col(2) += ds(i) * Eigen::Map<Eigen::VectorXd>(k.data(), x_num * y_num);
                 }
 
                 if (x.data() == y.data()) {
-                    r = 2 * _sn2 * Eigen::MatrixXd::Identity(x_samples, y_samples);
-                    grad.col(1) = Eigen::Map<Eigen::VectorXd>(r.data(), x_samples * y_samples);
+                    k = 2 * _sn2 * Eigen::MatrixXd::Identity(x_num, y_num);
+                    g.col(1) = Eigen::Map<Eigen::VectorXd>(k.data(), x_num * y_num);
                 }
                 else
-                    grad.col(1).setZero();
+                    g.col(1).setZero();
 
-                return grad;
+                return g;
             }
 
             template <typename... Args>
             void addPair(const double& value, const Function& function, Args... args)
             {
-                _pairs.insert(std::make_pair(value, function));
-
                 // Insert eigenvalue
                 _d.conservativeResize(_d.rows() + 1);
                 _d(_d.rows() - 1) = value;
@@ -171,23 +151,17 @@ namespace kernel_lib {
             // Eigenfunctions
             std::vector<Function> _f;
 
-            // Eigen pairs (eigenvalues, eigenfunctions)
-            // EigenFunction _f;
-            // This map can become ordered
-            std::unordered_map<double, Function> _pairs;
-
             // Spectral density
             virtual void spectral()
             {
                 _s = (-0.5 * std::pow(_l, 2) * _d).array().exp();
-                _s *= _sf2 / _s.sum();
+                _s /= _s.sum();
             }
 
             // Gradient spectral density
             virtual Eigen::VectorXd spectralGrad() const
             {
-                Eigen::VectorXd ds = _l * _s;
-                return _s.array() * (ds.sum() - ds.array());
+                return std::pow(_l, 2) * _s.array() * ((_d.array() * _s.array()).sum() - _d.array());
             }
 
             Eigen::VectorXd parameters() const override
@@ -198,7 +172,10 @@ namespace kernel_lib {
             /* Set specific kernel parameters */
             void setParameters(const Eigen::VectorXd& params) override
             {
+                // Set kernel length
                 _l = std::exp(params(0));
+                // Update kernel length
+                spectral();
             }
 
             /* Get number of parameters for the specific kernel */
